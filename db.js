2167b9e0ebb548f68b170647bce2e967ad05d7e7
Carlos0550 <carlospelinski05@gmail.com>
2024-06-08 10:26:13 -0300

    .

--- db.js
+++ db.js
@@ -1,26 +1,41 @@
 const mysql = require("mysql");
-const {
-    DB_HOST,
-    DB_NAME,
-    DB_PASSWORD,
-    DB_USER,
-    DB_PORT
-}= require("./config.js");
+const { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER, DB_PORT } = require("./config.js");
 
-const connection = mysql.createConnection({
+let connection;
+
+// Función para conectar inicialmente y manejar reconexiones
+function connectDatabase() {
+  connection = mysql.createConnection({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
     port: DB_PORT,
-    database: DB_NAME
-});
+    database: DB_NAME,
+    multipleStatements: true // Permite ejecutar múltiples declaraciones en una sola consulta
+  });
 
-connection.connect((err)=>{
+  // Conectar a la base de datos
+  connection.connect((err) => {
     if (err) {
-        console.log("error connecting: " + err.stack);
-        return
+      console.error("Error connecting to database:", err.stack);
+      setTimeout(connectDatabase, 2000); // Intentar reconectar después de 2 segundos
+    } else {
+      console.log("Connected to database");
     }
-    console.log("connected as id " + connection.threadId);
-});
+  });
 
-module.exports = connection
+  // Manejar errores de conexión perdida
+  connection.on('error', (err) => {
+    console.error('Database error:', err.message);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      connectDatabase(); // Reconectar si se pierde la conexión
+    } else {
+      throw err;
+    }
+  });
+}
+
+// Llamar a la función para conectar inicialmente
+connectDatabase();
+
+module.exports = connection;
